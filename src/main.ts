748743a16fb08d83a279746ee0e4bc8925133548
Mae Saltz <76541897+kirilbud@users.noreply.github.com>
2025-10-17 01:08:37 +0000

    added drawing and clear button (step 2 completed)

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,42 @@ const canvas = document.createElement("canvas");
 canvas.width = 256;
 canvas.height = 256;
 document.body.append(canvas);
+const ctx = canvas.getContext("2d");
+
+// Creating a clear button
+const clearButton = document.createElement("button");
+clearButton.innerHTML = "clear";
+document.body.appendChild(clearButton);
+
+//
+clearButton.addEventListener("click", () => {
+  if (ctx) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+});
+
+// Draw
+const cursor = { active: false, x: 0, y: 0 };
+
+canvas.addEventListener("mousedown", (e) => {
+  cursor.active = true;
+  cursor.x = e.offsetX;
+  cursor.y = e.offsetY;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (cursor.active) {
+    if (ctx) {
+      ctx.beginPath();
+      ctx.moveTo(cursor.x, cursor.y);
+      ctx.lineTo(e.offsetX, e.offsetY);
+      ctx.stroke();
+    }
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+  }
+});
+
+canvas.addEventListener("mouseup", () => {
+  cursor.active = false;
+});
